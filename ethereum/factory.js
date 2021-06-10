49138c4a9837f23e39f4cb8955ce006f07b0fe6f
circletakesthesquare <13kochd@gmail.com>
2021-06-09 20:10:54 -0400

    redeployed contract

--- ethereum/factory.js
+++ ethereum/factory.js
@@ -4,7 +4,7 @@ import CampaignFactory from './build/CampaignFactory.json';
 const instance = new web3.eth.Contract(
 
   JSON.parse(CampaignFactory.interface),
-  '0xA82975D19fA72a4fED32545f1a63d99C83FcAbC8'
+  '0x50d38Da51E2C697D4394f7663750B68aDffbb155'
 );
 
 export default instance;
